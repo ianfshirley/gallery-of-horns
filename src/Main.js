a5ff2d5109d1f434770c10f42d1a5d7d7ffc2691
ianfshirley <ianfshirley88@gmail.com>
2022-11-07 17:22:27 -0800

    added 2nd beast

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import trolloc from './trolloc.jpeg';
+// import trolloc from './trolloc.jpeg';
 
 
 class Main extends React.Component {
@@ -9,12 +9,19 @@ class Main extends React.Component {
       <main>
         <HornedBeast 
           title='Trolloc'
-          // src='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-          // src="https://i.ibb.co/YTR1M4V/trolloc.jpg"
-          src={trolloc}
+          src='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
+          src="https://i.ibb.co/YTR1M4V/trolloc.jpg"
+          // src={trolloc}
           alt='Trolloc'
           imgTitle='Trolloc'
           description='Shadowspawn, a horrifying cross of human and animal. Predators who delight in killing. Avoid at all costs.'
+        />
+        <HornedBeast
+          title='Unicorn'
+          src="https://i.ibb.co/ssw3VNt/unicorn.jpg"
+          alt="Unicorn"
+          imgTitle='Unicorn'
+          description='Majestic creature with magical abilities. Extremely rare.'
         />
       </main>
     )
