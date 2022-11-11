3ee48379e490aec69163bc637c87bc3764835f21
ianfshirley <ianfshirley88@gmail.com>
2022-11-10 20:03:00 -0800

    form filters beasts by number of horns

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,11 @@ import React from 'react';
 import HornedBeast from './HornedBeast';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import data from '../data.json';
-import ListGroup from 'react-bootstrap/ListGroup';
+// import data from '../data.json';
+// import ListGroup from 'react-bootstrap/ListGroup';
 import Form from 'react-bootstrap/Form';
+
+// console.log(data[0]);
 
 class Main extends React.Component {
 
@@ -15,6 +17,23 @@ class Main extends React.Component {
     }
   }
 
+  handleSelect = (event) => {
+    let hornData = '';
+    console.log(event.target.value)
+    if (event.target.value === 'one') {
+      hornData = this.props.data.filter(beastHorns => beastHorns.horns === 1)
+    } else if (event.target.value === 'two') {
+      hornData = this.props.data.filter(beastHorns => beastHorns.horns === 2)
+    } else if (event.target.value === 'three') {
+      hornData = this.props.data.filter(beastHorns => beastHorns.horns === 3)
+    } else if (event.target.value === 'hundred') {
+      hornData = this.props.data.filter(beastHorns => beastHorns.horns === 100)
+    } else {
+      hornData = this.props.data;
+    }
+    this.setState({filteredData: hornData});
+  }
+
   render() {    
     return (
       <main>
@@ -23,7 +42,7 @@ class Main extends React.Component {
             <Form.Label>
               Filter Beasts by Number of Horns
             </Form.Label>
-            <Form.Select>
+            <Form.Select onChange={this.handleSelect}>
               <option>All</option>
               <option value='one'>1</option>
               <option value='two'>2</option>
@@ -34,7 +53,7 @@ class Main extends React.Component {
         </Form>
         <Container>
           <Row>
-            {this.props.data.map((beast, idx) => 
+            {this.state.filteredData.map((beast, idx) => 
               <HornedBeast
                 _id={beast.name}
                 image_url={beast.image_url}
